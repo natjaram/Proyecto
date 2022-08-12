2bbc0dd3a064ab863be9dd25da81028abca4b4f5
natjaram <natjaram@bancolombia.com.co>
2022-08-12 13:00:05 -0500

    Clase Microservicios

---
+++ src/main/java/com/example/library/controller/GeneralesController.java
@@ -0,0 +1,52 @@
+package com.example.library.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.library.model.Libreria;
+
+@RestController
+@RequestMapping({"generales"})
+public class GeneralesController {
+    private Libreria libreria;
+
+    public GeneralesController()
+    {
+        libreria = new Libreria();
+        libreria.setNombre("Libreria Central");
+        libreria.setDireccion("Cerca del centro");
+        libreria.setTelefono("1234567890");
+        libreria.setHorario("9 - 18");
+    }
+
+    @GetMapping("/")
+    public Libreria info()
+    {
+        return libreria;
+    }
+
+    @GetMapping("/nombre")
+    public String nombre()
+    {
+        return libreria.getNombre();
+    }
+
+    @GetMapping("/direccion")
+    public String direccion()
+    {
+        return libreria.getDireccion();
+    }
+
+    @GetMapping("/telefono")
+    public String telefono()
+    {
+        return libreria.getTelefono();
+    }
+
+    @GetMapping("/horario")
+    public String horario()
+    {
+        return libreria.getHorario();
+    }
+}
